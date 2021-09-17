c939cc7ba49fd782ac9a1a91c3c943b13ab422e1
Connor Park <kaki104@hanmail.net>
2021-09-17 18:18:11 +0900

    Add project files.

---
+++ KakiPrismUnoMigration/KakiPrismUnoMigration.Shared/ViewModels/ShellViewModel.cs
@@ -0,0 +1,15 @@
+﻿using KakiPrismUnoMigration.Core.Mvvm;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+
+namespace KakiPrismUnoMigration.Shared.ViewModels
+{
+    public class ShellViewModel : ViewModelBase
+    {
+        public ShellViewModel()
+        {
+            Title = "Main Page";
+        }
+    }
+}
---
+++ KakiPrismUnoMigration/Platforms/KakiPrismUnoMigration.Wasm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Windows.UI.Xaml;
+
+namespace KakiPrismUnoMigration.Wasm
+{
+    public class Program
+    {
+        private static App _app;
+
+        static int Main(string[] args)
+        {
+            Windows.UI.Xaml.Application.Start(_ => _app = new App());
+
+            return 0;
+        }
+    }
+}
